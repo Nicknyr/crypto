b964928ff81d8a171ea3f212ec22b36ac3a89b9a
Nick Kinlen <nick_kinlen@hotmail.com>
2018-01-26 23:47:34 -0500

    First commit. Basic API request showing info on coins done

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from "axios";
+var NumberFormat = require('react-number-format');
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      coinList: []
+    };
+  }
+
+
+
+  componentDidMount() {
+    axios.get(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,LTC,ETH,IOT,TRX&tsyms=USD`)
+    .then(res => {
+      const coins = res.data;
+      console.log(coins);
+      this.setState({ coinList: coins});
+    });
+  }
+
+
+
+// Object.keys is used to map through the data. Can't map through the data without this because the data is not an array. Map can only be used on arrays.
+  render() {
+    return (
+      <div className="App">
+        {Object.keys(this.state.coinList).map((key) => (
+          <div className="container">
+            <span className="left">{key}</span>
+            <span className="right"><NumberFormat value={this.state.coinList[key].USD} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'$'} /></span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
