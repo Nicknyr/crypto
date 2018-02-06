807d761a9df15c5d725f6b3c0238087572061265
Nick Kinlen <nick_kinlen@hotmail.com>
2018-02-05 19:19:09 -0500

    Got D3 component set up and displaying properly

--- src/d3-2.js
+++ src/d3-2.js
@@ -4,63 +4,74 @@ import * as d3 from 'd3'
 
 class Chart extends React.Component {
 
-  var url = "https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=200&aggregate=3&e=CCCAGG";
+  componentDidMount() {
+    var url = "https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=200&aggregate=3&e=CCCAGG";
 
-      // This needs to be changed to my API request
-  d3.json(url).get(function(error, d) {
+        // This needs to be changed to my API request
+    d3.json(url).get(function(error, d) {
 
-  	var data = d.Data;
-    data.forEach(function(d){ d.time = new Date(d.time * 1000) });
+    	var data = d.Data;
+      data.forEach(function(d){ d.time = new Date(d.time * 1000) });
 
-    if (error) throw error;
+      if (error) throw error;
 
-    var svg = d3.select("svg"),
-        margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      var svg = d3.select("svg"),
+          margin = {top: 20, right: 20, bottom: 30, left: 50},
+          width = +svg.attr("width") - margin.left - margin.right,
+          height = +svg.attr("height") - margin.top - margin.bottom,
+          g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var parseTime = d3.timeParse("%d-%b-%y");
+      var parseTime = d3.timeParse("%d-%b-%y");
 
-    var x = d3.scaleTime()
-        .rangeRound([0, width]);
+      var x = d3.scaleTime()
+          .rangeRound([0, width]);
 
-    var y = d3.scaleLinear()
-        .rangeRound([height, 0]);
+      var y = d3.scaleLinear()
+          .rangeRound([height, 0]);
 
-    var line = d3.line()
-        .x(function(d) { return x(d.time); })
-        .y(function(d) { return y(d.close); });
+      var line = d3.line()
+          .x(function(d) { return x(d.time); })
+          .y(function(d) { return y(d.close); });
 
-    x.domain(d3.extent(data, function(d) { return d.time; }));
-    y.domain(d3.extent(data, function(d) { return d.close; }));
+      x.domain(d3.extent(data, function(d) { return d.time; }));
+      y.domain(d3.extent(data, function(d) { return d.close; }));
 
-    g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-      .select(".domain")
-        .remove();
+      g.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x))
+        .select(".domain")
+          .remove();
 
-    g.append("g")
-        .call(d3.axisLeft(y))
-      .append("text")
-        .attr("fill", "#000")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("Price ($)");
+      g.append("g")
+          .call(d3.axisLeft(y))
+        .append("text")
+          .attr("fill", "#000")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", "0.71em")
+          .attr("text-anchor", "end")
+          .text("Price ($)");
 
-    g.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "#FFB041")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 2)
-        .attr("d", line);
-  });
+      g.append("path")
+          .datum(data)
+          .attr("fill", "none")
+          .attr("stroke", "#FFB041")
+          .attr("stroke-linejoin", "round")
+          .attr("stroke-linecap", "round")
+          .attr("stroke-width", 2)
+          .attr("d", line);
+    });
 
+  }
+
+  render() {
+      return (
+      <div className="cointaner">
+         <svg width="960" height="500" ref={d => (this.svg = d)}/>
+      </div>
+
+     )
+    }
 }
 
 export default Chart;
