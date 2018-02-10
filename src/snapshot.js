4973a6d4f4ba9d795b5284b39fc051d70553bc8a
Nick Kinlen <nick_kinlen@hotmail.com>
2018-02-10 01:26:49 -0500

    Snapshot component retrieving coin data from coinsnapshotbyfulid API request and setting data as state in CoinInfo component

--- src/snapshot.js
+++ src/snapshot.js
@@ -7,18 +7,34 @@ class CoinInfo extends React.Component {
     super(props);
 
     this.state = {
-      coinInfo: []
+      coinName: '',
+      coinImage: '',
+      coinDescription: '',
+      coinFeatures: '',
+      coinTech: '',
+      coinSupply: '',
+      coinMined: '',
+      website: ''
     }
   }
 
-  //https://cors-anywhere.herokuapp.com/
+  // Adding https://cors-anywhere.herokuapp.com/ to API URL fixed the CORS error
   //https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/?id=1182
   componentDidMount() {
     axios.get(`https://cors-anywhere.herokuapp.com/https://www.cryptocompare.com/api/data/coinsnapshotfullbyid/?id=1182`)
     .then(res => {
-      const info = res.data;
-      this.setState({ coinInfo: info});
-      console.log(info);
+        const info = res.data.Data.General;
+        this.setState({
+          coinName: info.Name,
+          coinImage: info.ImageUrl,
+          coinDescription: info.Description,
+          coinFeatures: info.Features,
+          coinTech: info.Technology,
+          coinSupply: info.TotalCoinSupply,
+          coinMined: info.TotalCoinsMined,
+          website: info.Website
+        });
+        console.log(this.state);
     })
     .catch(error => {
       console.log(error);
